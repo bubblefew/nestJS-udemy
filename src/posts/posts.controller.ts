92c2df94ef7ef52e53e1ffb0c0b0f1932714df04
DESKTOP-A26APKO\bubbl <bubble.few@gmail.com>
2024-10-26 12:19:57 +0700

    feat: 56 Mapped Type Using Swaage and PartialType

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -13,8 +14,29 @@ export class PostsController {
     return this.postsService.findAll(userId);
   }
 
+  @ApiOperation({
+    summary: 'Create a new post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The post has been successfully created.',
+  })
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     console.log(createPostDto);
+  }
+
+  @ApiOperation({
+    summary: 'Updates an existing post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The post has been successfully updated.',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostDto: PatchPostDto) {
+    console.log(patchPostDto);
+
+    return 'Update post';
   }
 }
